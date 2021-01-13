e3d84019eaf533fb34ffda43d34a5c6b4ea093ad
ww8007 <wshmin@naver.com>
2021-01-13 22:57:19 +0900

    add header

--- react-forum/src/App.js
+++ react-forum/src/App.js
@@ -1,15 +1,35 @@
+import React from 'react';
 import './App.css';
-
+import UserAsync from './UserAsync';
 function App() {
   return (
-    <header>
+    <>
       <div className="header">
-        <div className="logo">
-          <a href="#">시크릿쥬쥬</a>
-          <span>a</span>
+        <div className="logo">시크릿 쥬쥬</div>
+        <div className="menu">
+          <div className="topMenu">
+            <div>
+              <a href="">로그인</a>
+            </div>
+            <div>
+              <a href="">회원가입</a>
+            </div>
+          </div>
+          <div className="mainMenu">
+            <div>
+              <a href="">게시판</a>
+            </div>
+            <div>
+              <a href="">커뮤니티</a>
+            </div>
+            <div>
+              <a href="">Q&A</a>
+            </div>
+          </div>
         </div>
       </div>
-    </header>
+      <body>{/* <UserAsync></UserAsync> */}</body>
+    </>
   );
 }
 
