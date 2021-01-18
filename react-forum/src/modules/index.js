1de405aee3d804be23c2a3c6b1af1071dbc371ce
ww8007 <wshmin@naver.com>
2021-01-18 22:07:01 +0900

    rootReducer add loading

--- react-forum/src/modules/index.js
+++ react-forum/src/modules/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
-
-const rootReudcer = combineReducers({ auth });
+import loading from './loading';
+const rootReudcer = combineReducers({ auth, loading });
 
 export default rootReudcer;
