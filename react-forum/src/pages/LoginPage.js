0bf011aecdadf53bca217e851df434a9a5a72a02
ww8007 <wshmin@naver.com>
2021-01-18 21:36:22 +0900

    Use LoginForm

--- react-forum/src/pages/LoginPage.js
+++ react-forum/src/pages/LoginPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import AuthForm from '../components/auth/AuthForm';
 import AuthTemplate from '../components/auth/AuthTemplate';
+import LoginForm from '../components/auth/LoginForm';
 const LoginPage = () => {
   return (
     <AuthTemplate>
-      <AuthForm type="login"></AuthForm>
+      <LoginForm></LoginForm>
     </AuthTemplate>
   );
 };
