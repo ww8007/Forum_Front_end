2bf58344ae776d92b75924c016045985679d01b0
ww8007 <wshmin@naver.com>
2021-01-19 01:42:42 +0900

    Mover to container/auth directory

---
+++ react-forum/src/containers/auth/RegisterForm.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm, register } from '../../modules/auth';
+import AuthForm from '../../components/auth/AuthForm';
+
+const RegisterForm = () => {
+  const dispatch = useDispatch();
+  const { form, auth, authError } = useSelector(({ auth }) => ({
+    form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+  }));
+  // input 변경 이벤트 핸들러
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(
+      changeField({
+        form: 'register',
+        key: name,
+        value,
+      }),
+    );
+  };
+  // 폼 등록 이벤트 핸들러
+  const onSubmit = (e) => {
+    e.prevenDefault();
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      // TODO 오류처리
+      return;
+    }
+    dispatch(register({ username, password }));
+  };
+
+  // 컴포넌트 최초 랜더링 form 초기화
+
+  useEffect(() => {
+    dispatch(initializeForm('register'));
+  }, [dispatch]);
+
+  // 회원가입 성공/실패 처리
+  useEffect(() => {
+    if (authError) {
+      console.log('오류발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('회원가입 성공');
+      console.log(auth);
+    }
+  }, [auth, authError]);
+
+  return (
+    <AuthForm
+      type="register"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+// from 등록 이벤트 핸들러
+
+export default RegisterForm;
