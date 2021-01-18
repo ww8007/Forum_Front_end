75a5c61dd675d00868c35465d8c0975dabaa2165
ww8007 <wshmin@naver.com>
2021-01-19 01:45:48 +0900

    Add rootReducer

--- react-forum/src/modules/index.js
+++ react-forum/src/modules/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import auth, { authSaga } from './auth';
 import loading from './loading';
-const rootReudcer = combineReducers({ auth, loading });
+
+const rootReducer = combineReducers({
+  auth,
+  loading,
+});
 
 export function* rootSaga() {
-  yield all([authSaga]);
+  yield all([authSaga()]);
 }
 
-export default rootReudcer;
+export default rootReducer;
