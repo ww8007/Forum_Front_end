3f1aba78c4b1edf121a8e542a07735fa0610bbaf
ww8007 <wshmin@naver.com>
2021-01-18 21:11:56 +0900

    Make LoginForm

--- react-forum/src/pages/RegisterPage.js
+++ react-forum/src/pages/RegisterPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import AuthForm from '../components/auth/AuthForm';
+import AuthTemplate from '../components/auth/AuthTemplate';
 const RegisterPage = () => {
-  return <div>회원가입</div>;
+  return (
+    <AuthTemplate>
+      <AuthForm type="register"></AuthForm>
+    </AuthTemplate>
+  );
 };
 
 export default RegisterPage;
