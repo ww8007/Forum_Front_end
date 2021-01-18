270526c0565cc5d3d164a4eddc6d5bfa6eb91bc3
ww8007 <wshmin@naver.com>
2021-01-18 23:41:56 +0900

    Make RootSaga

--- react-forum/src/modules/index.js
+++ react-forum/src/modules/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
-import auth from './auth';
+import { all } from 'redux-saga/effects';
+import auth, { authSaga } from './auth';
 import loading from './loading';
 const rootReudcer = combineReducers({ auth, loading });
+
+export function* rootSaga() {
+  yield all([authSaga]);
+}
 
 export default rootReudcer;
