07f0ce99f87ef087b8a6dae12a2b810bfa952097
ww8007 <wshmin@naver.com>
2021-01-18 23:30:38 +0900

    Repacktoring action types

--- react-forum/src/modules/auth.js
+++ react-forum/src/modules/auth.js
@@ -1,8 +1,26 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import { createRequestActionTypes } from '../lib/createRequestSaga';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+// 아래 const 여러번 리펙토링
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+  'auth/REGISTER',
+);
+
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+  'auth/LOGIN',
+);
+
+// const REGISTER = 'auth/REGISTER';
+// const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+// const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+
+// const LOGIN = 'auth/LOGIN';
+// const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+// const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
 
 export const changeField = createAction(
   CHANGE_FIELD,
