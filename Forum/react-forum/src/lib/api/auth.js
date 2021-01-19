9b36b694f2649b13aa7670be60dceef651c8212c
ww8007 <wshmin@naver.com>
2021-01-19 23:41:43 +0900

    Change API Address

--- Forum/react-forum/src/lib/api/auth.js
+++ Forum/react-forum/src/lib/api/auth.js
@@ -1,15 +1,13 @@
 import client from './client';
+import qs from 'qs';
 
 // 로그인
 export const login = ({ username, password }) =>
-  client.post('/api/auth/login', { username, password });
+  client.post('login', qs.stringify({ username, password }));
 
 // 회원가입
 export const register = ({ username, email, password }) =>
-  client.post(
-    'http://djangocrudboard-env-2.eba-pncegi8j.ap-northeast-2.elasticbeanstalk.com/register',
-    { username, email, password },
-  );
+  client.post('/register', qs.stringify({ username, email, password }));
 
 // 로그인 상태 확인
 export const check = () => client.get('/api/auth/check');
