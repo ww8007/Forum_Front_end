b8e26b541c919f9faf9dacf4d3d3dfbcc8aa7031
ww8007 <wshmin@naver.com>
2021-01-19 23:42:55 +0900

    css base

---
+++ Forum/react-forum/src/components/common/Responsive.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// const ResposiveBlock = styled.div`
+//     padding-left : 1rem;
+//     padding-right : 1rem;
+//     width: 1024px;
+//     margin : 0 auto;
+
+//     @media
