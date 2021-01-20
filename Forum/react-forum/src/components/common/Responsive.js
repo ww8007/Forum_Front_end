4eab2b24487ce4611babab70bba875641f9c26bd
ww8007 <wshmin@naver.com>
2021-01-20 23:29:24 +0900

    Make ResposiveBlock

--- Forum/react-forum/src/components/common/Responsive.js
+++ Forum/react-forum/src/components/common/Responsive.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// const ResposiveBlock = styled.div`
-//     padding-left : 1rem;
-//     padding-right : 1rem;
-//     width: 1024px;
-//     margin : 0 auto;
+const ResposiveBlock = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  width: 1024px;
+  margin: 0 auto; // 중앙 정렬
+  // 브라우저 크기 따라 반응
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
 
-//     @media
+const Reponsive = ({ children, ...rest }) => {
+  //...rest 사용 ResponsiveBlock 전달
+  return <ResposiveBlock {...rest}>{children}</ResposiveBlock>;
+};
+
+export default Reponsive;
